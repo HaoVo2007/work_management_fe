d11e19b75565b3eb06e0343e4f8ace7b9e5a052b
provided <provided@provideds-Mac-mini.local>
2025-10-24 18:22:59 +0700

    push code

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,17 @@
 <template>
+  <Header v-if="showHeader" />
   <router-view />
 </template>
 
 <script setup>
+import Header from './components/Header.vue';
+import { computed } from 'vue';
+import { useRoute } from 'vue-router';
 
+const route = useRoute();
+const showHeader = computed(() => {
+  return !['/login', '/register'].includes(route.path)
+})
 </script>
 
 <style>
